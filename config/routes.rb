143dd343bfea2165b4c1dab0d679762ecf6e27ee
Jahwffrey <johnthomason@umail.ucsb.edu>
2014-05-08 14:44:36 -0700

    so scaffold worked

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 FirstApp::Application.routes.draw do
+  get "home/index"
+  resources :users
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
